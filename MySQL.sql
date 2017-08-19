14d832b31aae727a632c5f4d51170d944a324d9e
Dragonair <blizzardproject.tk@gmail.com>
2017-08-18 22:26:43 -0400

    Delete MySQL.sql

--- MySQL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  `email` text NOT NULL,
-  `ip` text NOT NULL,
-  `gamertag` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
